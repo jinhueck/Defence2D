0a673af4ad459e89d4bfc85c0e55035aa94776ba
ybeom94 <ybeom94@nate.com>
2021-05-19 21:05:34 +0900

    Enemy

---
+++ Defence2D/Assets/02.Scripts/EliteEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EliteEnemy : EnemyCtrl
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetInfo();
+
+        StartCoroutine(Beat());
+    }
+
+    void SetInfo()
+    {
+        beat = 0.5f;
+        speed = 10.0f;
+        distance = 0.4f;
+        MovePos = new Vector2(-1.8f, 5.4f);
+    }
+
+    void MoveCheck()
+    {
+        MovePos.y -= distance;
+    }
+
+    IEnumerator Beat()
+    {
+        while (state == State.MOVE)
+        {
+            MoveCheck();
+            yield return new WaitForSeconds(beat);
+        }
+    }
+}
---
+++ Defence2D/Assets/02.Scripts/GroupEnemy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupEnemy : EnemyCtrl
+{
+    private bool isRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetInfo();
+
+        StartCoroutine(Beat());
+    }
+
+    void SetInfo()
+    {
+        beat = 1.0f;
+        speed = 10.0f;
+        distance = 0.2f;
+        MovePos = new Vector2(0f, 4f);
+    }
+
+
+    void MoveCheck()
+    {
+        if (isRight)
+        {
+            if (MovePos.x >= 0.9f)
+            {
+                MovePos.y -= distance;
+                isRight = !isRight;
+            }
+            else
+                MovePos.x += distance;
+        }
+        else
+        {
+            if (MovePos.x <= -0.9f)
+            {
+                MovePos.y -= distance;
+                isRight = !isRight;
+            }
+            else
+                MovePos.x -= distance;
+        }
+        //Debug.Log(MovePos.x);
+    }
+
+    IEnumerator Beat()
+    {
+        while (state == State.MOVE)
+        {
+            MoveCheck();
+            yield return new WaitForSeconds(beat);
+        }
+    }
+}
