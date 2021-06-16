30132619948846e019868c2935c66ffe6e5557b8
burgii <qorgh9184@gmail.com>
2021-06-17 00:05:32 +0900

    Character SOFile 정보 추가 및 SOFile Storage 추가

---
+++ Defence2D/Assets/02.Scripts/SOFileStorage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SOFileStorage : MonoBehaviour
+{
+    public Dictionary<string, SOFile_CharacterData> map_CharacterData = new Dictionary<string, SOFile_CharacterData>();
+
+    private void Awake()
+    {
+        Setup();
+    }
+
+    public void Setup()
+    {
+        var p_listCharacterData = Resources.LoadAll<SOFile_CharacterData>("SOFile/CharacterData");
+        lock(p_listCharacterData)
+        {
+            for (int i = 0; i < p_listCharacterData.Length; i++)
+            {
+                SOFile_CharacterData p_characterData = p_listCharacterData[i];
+                map_CharacterData.Add(p_characterData.szCode, p_characterData);
+            }
+        }
+    }
+}
---
+++ Defence2D/Assets/02.Scripts/SOFile_CharacterData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SOFile_CharacterStat", menuName = "Scriptable Object/SOFile_CharacterStat", order = int.MaxValue)]
+public class SOFile_CharacterData : ScriptableObject
+{
+    public int nIndex;
+    public string szCode;
+    public int nUnitType;
+    public int nUnitGrade;
+    public int nJob;
+    public float fDamage;
+    public float fGrowth_Damage;
+    public float fDefense;
+    public float fGrowth_Defense;
+    public float fHealth;
+    public float fGrowth_Health;
+    public float fAttackRange;
+    public float fMoveSpeed;
+    public float fSpawnCoolTime;
+    public int fPrice;
+    public string szResourceName;
+    public float fCharacterScale;
+    public float fSkillCoolTime;
+    public string szSkillCode;
+    public int nGold;
+}
